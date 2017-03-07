e67b329a7271aa024ee28f1d355555d6eb8c91a8
John Michael McCasland <jmmccasland@gmail.com>
2017-03-07 14:13:21 -0600

    Live text input with vue

---
+++ app/application.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="app">
+    <h1>{{ message }}</h1>
+    <input v-model="message">
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World',
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
--- app/index.js
+++ app/index.js
@@ -1,1 +1,6 @@
 import 'whatwg-fetch';
+import Vue from 'vue';
+import  App  from './application.vue';
+
+const app = new Vue(App);
+app.$mount('.app');
